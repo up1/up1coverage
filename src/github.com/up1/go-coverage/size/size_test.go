6a65385a2ce61d72f3c7f4eeb7081ea26c0f9b2d
Somkiat <somkiat.p@gmail.com>
2014-05-12 13:49:36 +0700

    Update file

--- src/github.com/up1/go-coverage/size/size_test.go
+++ src/github.com/up1/go-coverage/size/size_test.go
@@ -1,17 +1,22 @@
 package size
 
-func Size(a int) string {
-	switch {
-	case a < 0:
-		return "negative"
-	case a == 0:
-		return "zero"
-	case a < 10:
-		return "small"
-	case a < 100:
-		return "big"
-	case a < 1000:
-		return "huge"
-	}
-	return "enormous"
+import "testing"
+
+type Test struct {
+    in  int
+    out string
+}
+
+var tests = []Test{
+    {-1, "negative"},
+    {5, "small"},
+}
+
+func TestSize(t *testing.T) {
+    for i, test := range tests {
+        size := Size(test.in)
+        if size != test.out {
+            t.Errorf("#%d: Size(%d)=%s; want %s", i, test.in, size, test.out)
+        }
+    }
 }
